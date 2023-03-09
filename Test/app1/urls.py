0419e5101f342f8908483a38d270ca95e28a7cbf
hobrus <bhobrus@gmail.com>
2023-03-09 20:44:26 +0300

    lesson 2

--- Test/app1/urls.py
+++ Test/app1/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello2)
+    path('hello/', views.hello),
+    path('site/', views.site)
 ]
--- Test/app1/views.py
+++ Test/app1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 
@@ -8,9 +9,13 @@ def hello(request):
     return HttpResponse("Hello, world!")
 
 
-def hello2(request):
-    name = "Иванов И.П."
-    return HttpResponse(f'''
-<h1>"Изучаем django"</h1>
-<strong>Автор</strong>: <i>{name}</i>
-''')
+def site(request):
+    items = [
+        {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
+        {"id": 2, "name": "Куртка кожаная", "quantity": 2},
+        {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
+        {"id": 7, "name": "Картофель фри", "quantity": 0},
+        {"id": 8, "name": "Кепка", "quantity": 124},
+    ]
+    context = {'goods': items}
+    return render(request, 'main.html', context)
